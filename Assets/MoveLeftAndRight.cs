056e2329bc320f390e84957df648b6a94f34dc19
HEPHAESTUS\Josh <josh.folsom@yahoo.com>
2016-09-10 14:45:05 -0700

    Initial commit

---
+++ Assets/MoveLeftAndRight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftAndRight : MonoBehaviour
+{
+	public float speed = 1.0f;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//moves object left or right with modifiers of directional input, speed and time rather than frames
+		transform.position += Vector3.right * Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+	}
+}
